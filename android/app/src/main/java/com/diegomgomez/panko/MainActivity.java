8d3e7845abf76d1944012d0568bf32459e9924a4
Diego M. Gomez <diego.morales@qustodio.com>
2017-01-28 22:25:58 -0800

    Initial Panko Android app commit

---
+++ android/app/src/main/java/com/diegomgomez/panko/MainActivity.java
@@ -0,0 +1,13 @@
+package com.diegomgomez.panko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
